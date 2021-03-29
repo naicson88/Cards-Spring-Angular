a7f7eaab56e0c62c4b4385b0a6aca06485c2db55
naicson88 <naicson88@hotmail.com>
2021-03-28 23:44:42 -0300

    estats

--- yugioh-back/src/test/java/com/naicson/yugioh/YugiohApplicationTests.java
+++ yugioh-back/src/test/java/com/naicson/yugioh/YugiohApplicationTests.java
@@ -1,13 +1,34 @@
 package com.naicson.yugioh;
 
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YugiohApplicationTests {
-
+	
+	Calculator teste = new Calculator();
+	
 	@Test
-	void contextLoads() {	
+	void contextLoads() {
+		//given
+		int numberOne = 20;
+		int numberTwo = 15;
+		
+		//when
+		int result = teste.add(numberOne, numberTwo);
+		
+		//then
+		assertThat(result).isEqualTo(35);
+	}
+	
+	class Calculator{
+		int add(int a, int b) {
+			return a + b;
+		}
 	}
 
 }
